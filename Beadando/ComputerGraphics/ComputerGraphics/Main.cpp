007059afc8011269a098959ffda60889eff66383
Márton Bán <marton.bn@gmail.com>
2021-05-03 01:11:20 +0200

    Window OpenGL ONE

--- Beadando/ComputerGraphics/ComputerGraphics/Main.cpp
+++ Beadando/ComputerGraphics/ComputerGraphics/Main.cpp
@@ -16,12 +16,22 @@ int main() {
 		return -1;
 	}
 
+	glfwMakeContextCurrent(window);
+	gladLoadGL();
+
+	glViewport(0, 0, 800, 800);
+
+	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glfwSwapBuffers(window);
+
+	
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
 	}
 
 
-	glfwMakeContextCurrent(window);
+	
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
