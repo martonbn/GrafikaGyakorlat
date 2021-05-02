9a914e043d17b7a38859c3857ef03d161a6b1c7e
Márton Bán <marton.bn@gmail.com>
2021-05-03 00:59:52 +0200

    Window Apperance

--- Beadando/ComputerGraphics/ComputerGraphics/Main.cpp
+++ Beadando/ComputerGraphics/ComputerGraphics/Main.cpp
@@ -4,5 +4,26 @@
 
 int main() {
 	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 800, "Hello OpenGL", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to crate Window";
+		glfwTerminate();
+		return -1;
+	}
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+
+	glfwMakeContextCurrent(window);
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 }
