56b52c7e1633486973e55553272f55114dfa9e8b
Márton Bán <marton.bn@gmail.com>
2021-05-03 01:24:06 +0200

    class work

---
+++ Gyakorlat/samples/matrices/matrix.c
@@ -0,0 +1,73 @@
+#include "matrix.h"
+
+#include <stdio.h>
+
+void init_zero_matrix(float matrix[3][3])
+{
+    int i;
+    int j;
+
+    for (i = 0; i < 3; ++i) {
+        for (j = 0; j < 3; ++j) {
+            matrix[i][j] = 0.0;
+        }
+    }
+}
+
+void init_identity_matrix(float matrix[3][3])
+{
+    int i;
+    int j;
+
+    for (i = 0; i < 3; ++i) {
+        for (j = 0; j < 3; ++j) {
+			if(i==j){
+				matrix[i][j] = 1.0;
+			}else{
+				matrix[i][j] = 0.0;
+			}
+            
+        }
+    }
+}
+
+
+void print_matrix(const float matrix[3][3])
+{
+    int i;
+    int j;
+
+    for (i = 0; i < 3; ++i) {
+        for (j = 0; j < 3; ++j) {
+            printf("%4.4f ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+	printf("\n");
+}
+
+
+void mult_by_scalar(float result[3][3], float scalar, const float matrix[3][3]){
+	int i;
+    int j;
+
+    for (i = 0; i < 3; ++i) {
+        for (j = 0; j < 3; ++j) {
+            result[i][j] = matrix[i][j]*scalar;
+        }
+    }
+}
+
+
+void add_matrices(const float a[3][3], const float b[3][3], float c[3][3])
+{
+    int i;
+    int j;
+
+    for (i = 0; i < 3; ++i) {
+        for (j = 0; j < 3; ++j) {
+            c[i][j] = a[i][j] + b[i][j];
+        }
+    }
+}
+
